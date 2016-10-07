0f740e7246f3c4071286cbdc0868501daefdcea7
Dale Bustad <dale@divmain.com>
2016-10-07 00:54:53 -0700

    Implement various loop nodes (listed below).
    ForInStatement
    DoWhileStatement
    LabeledStatement
    ContinueStatement
    BreakStatement

--- src/subgenerators/loops.js
+++ src/subgenerators/loops.js
@@ -9,3 +9,51 @@ exports.ForStatement = (node, generator) => {
   generator.advance(")");
   generator.generate(node.body);
 };
+
+// for (left in right) body
+exports.ForInStatement = (node, generator) => {
+  generator.advance("for(");
+  generator.generate(node.left);
+  generator.advance(" in ");
+  generator.generate(node.right);
+  generator.advance(")");
+  // node.body will always be a statement
+  generator.generate(node.body);
+};
+
+// do body while (test)
+exports.DoWhileStatement = (node, generator) => {
+  generator.advance("do ");
+  // node.body will always be a statement
+  generator.generate(node.body);
+  generator.advance("while(");
+  generator.generate(node.test);
+  generator.advance(")");
+};
+
+// label: body
+exports.LabeledStatement = (node, generator) => {
+  generator.generate(node.label);
+  generator.advance(":");
+  generator.generate(node.body);
+};
+
+// continue label;
+exports.ContinueStatement = (node, generator) => {
+  generator.advance("continue");
+  if (node.label) {
+    generator.advance(" ");
+    generator.generate(node.label);
+  }
+  generator.advance(";");
+};
+
+// break label;
+exports.BreakStatement = (node, generator) => {
+  generator.advance("break");
+  if (node.label) {
+    generator.advance(" ");
+    generator.generate(node.label);
+  }
+  generator.advance(";");
+};

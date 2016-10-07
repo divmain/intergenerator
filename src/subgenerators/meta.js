f1fe08d5bfd0baf8a4acaab8997f3267e3ecdd69
Dale Bustad <dale@divmain.com>
2016-10-07 00:23:39 -0700

    Support program directives (like "use strict").

--- src/subgenerators/meta.js
+++ src/subgenerators/meta.js
@@ -1,8 +1,22 @@
 exports.Program = (node, generator) => {
+  if (node.directives && node.directives.length) {
+    node.directives.forEach(directiveNode => {
+      generator.generate(directiveNode);
+    });
+  }
+
   // Program nodes should not have their own mapping.
   node.body.forEach(bodyNode => {
     generator.generate(bodyNode);
   });
+};
+
+exports.Directive = (node, generator) => {
+  generator.generate(node.value);
+};
+
+exports.DirectiveLiteral = (node, generator) => {
+  generator.advance(`"${node.value}";`);
 };
 
 exports.File = (node, generator) => {
